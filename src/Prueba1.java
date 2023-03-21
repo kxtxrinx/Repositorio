297e3180e144325d8dc9b1afaf1aca0da48f24fb
test <test@test.com>
2023-03-21 18:29:55 +0100

    Declaración de variable

--- src/Prueba1.java
+++ src/Prueba1.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Prueba1 {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		System.out.println("Inicio del proyecto");
 		
 		System.out.println("¿Cuál es tu nombre?");
-
+		String nombre = s.nextLine();
 
 	}
 
